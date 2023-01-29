5f7eab65bb87b0ccae7b3c06611af7c7e1ca27da
Dmitriy Tarasenko <kupperxd1@gmail.com>
2023-01-29 17:56:29 +0300

    init nuxt 3

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+<script lang="ts" setup>
+const config = useRuntimeConfig();
+
+console.log('config', {
+    config,
+});
+</script>
